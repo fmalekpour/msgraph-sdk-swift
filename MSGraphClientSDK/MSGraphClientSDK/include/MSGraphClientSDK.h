da71665f3190c95ae19d6fdf02dd446071b1ac52
Farhad Malekpour <fm@farhad.ca>
2026-05-04 22:57:03 -0700

    BF

--- MSGraphClientSDK/MSGraphClientSDK/include/MSGraphClientSDK.h
+++ MSGraphClientSDK/MSGraphClientSDK/include/MSGraphClientSDK.h
@@ -2,59 +2,60 @@
 // Copyright (c) Microsoft Corporation. All Rights Reserved. Licensed under the MIT License. See License in the project root for license information.
 //
 // This file is the umbrella header used by Swift Package Manager.
-// It uses flat imports (no module-qualified paths) so that all header search
-// paths configured in Package.swift can resolve them correctly.
+// All imports use paths relative to this file (which lives in include/) so that
+// Clang can resolve them through the publicHeadersPath without needing extra
+// header search paths on the consumer side.
 //
 
 #ifndef MSGraphClientSDK_h
 #define MSGraphClientSDK_h
 
 // Authentication
-#import "MSAuthenticationProvider.h"
-#import "MSAuthenticationProviderOptions.h"
-#import "MSALAuthenticationProvider.h"
-#import "MSALAuthenticationProviderOptions.h"
+#import "../Authentication/MSAuthenticationProvider.h"
+#import "../Authentication/MSAuthenticationProviderOptions.h"
+#import "../Authentication/MSAL/MSALAuthenticationProvider.h"
+#import "../Authentication/MSAL/MSALAuthenticationProviderOptions.h"
 
 // Middleware – Protocols
-#import "MSGraphMiddleware.h"
+#import "../Middleware/Protocols/MSGraphMiddleware.h"
 
 // Middleware – Options
-#import "MSMiddlewareOptions.h"
-#import "MSAuthenticationHandlerOptions.h"
-#import "MSRedirectHandlerOptions.h"
-#import "MSRetryHandlerOptions.h"
+#import "../Middleware/Options/MSMiddlewareOptions.h"
+#import "../Middleware/Options/MSAuthenticationHandlerOptions.h"
+#import "../Middleware/Options/MSRedirectHandlerOptions.h"
+#import "../Middleware/Options/MSRetryHandlerOptions.h"
 
 // Middleware – Implementations
-#import "MSAuthenticationHandler.h"
-#import "MSRedirectHandler.h"
-#import "MSRetryHandler.h"
-#import "MSHttpProvider.h"
-#import "MSURLSessionManager.h"
-#import "MSURLSessionTaskDelegate.h"
+#import "../Middleware/Implementations/Authentication/MSAuthenticationHandler.h"
+#import "../Middleware/Implementations/RedirectHandler/MSRedirectHandler.h"
+#import "../Middleware/Implementations/RetryHandler/MSRetryHandler.h"
+#import "../Middleware/Implementations/HTTPProvider/MSHttpProvider.h"
+#import "../Middleware/Implementations/HTTPProvider/MSURLSessionManager.h"
+#import "../Middleware/Implementations/HTTPProvider/MSURLSessionTaskDelegate.h"
 
 // HTTP Client
-#import "MSClientFactory.h"
-#import "MSMiddlewareFactory.h"
-#import "MSHTTPClient.h"
+#import "../HTTPClient/MSClientFactory.h"
+#import "../HTTPClient/MSMiddlewareFactory.h"
+#import "../HTTPClient/MSHTTPClient.h"
 
 // Common
-#import "MSConstants.h"
-#import "MSErrorCodes.h"
+#import "../Common/MSConstants.h"
+#import "../Common/MSErrorCodes.h"
 
 // Session Tasks
-#import "MSURLSessionTask.h"
-#import "MSURLSessionDataTask.h"
-#import "MSURLSessionDownloadTask.h"
-#import "MSURLSessionUploadTask.h"
+#import "../Common/Session Tasks/MSURLSessionTask.h"
+#import "../Common/Session Tasks/MSURLSessionDataTask.h"
+#import "../Common/Session Tasks/MSURLSessionDownloadTask.h"
+#import "../Common/Session Tasks/MSURLSessionUploadTask.h"
 
 // Batch Content
-#import "MSBatchRequestStep.h"
-#import "MSBatchRequestContent.h"
-#import "MSBatchResponseContent.h"
+#import "../GraphContent/BatchContent/MSBatchRequestStep.h"
+#import "../GraphContent/BatchContent/MSBatchRequestContent.h"
+#import "../GraphContent/BatchContent/MSBatchResponseContent.h"
 
 // Graph Tasks
-#import "MSPageIterator.h"
-#import "MSLargeFileUploadTask.h"
-#import "MSGraphOneDriveLargeFileUploadTask.h"
+#import "../GraphTasks/MSPageIterator.h"
+#import "../GraphTasks/MSLargeFileUploadTask.h"
+#import "../GraphTasks/MSGraphOneDriveLargeFileUploadTask.h"
 
 #endif /* MSGraphClientSDK_h */
