4081ab7f5083c993c1ef86a3966ea67b5dae8476
Farhad Malekpour <fm@farhad.ca>
2026-05-04 22:52:41 -0700

    BF

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MSGraphClientSDK",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v16),
         .macOS(.v11),
     ],
     products: [
